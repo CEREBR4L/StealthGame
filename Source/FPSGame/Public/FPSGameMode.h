0b8ed276f3b4841827e90c3d67eee522132e366f
Matt <mattmillergraphics@gmail.com>
2018-03-17 22:08:41 +0000

    Remove control from player on win

--- Source/FPSGame/Public/FPSGameMode.h
+++ Source/FPSGame/Public/FPSGameMode.h
@@ -14,6 +14,11 @@ class AFPSGameMode : public AGameModeBase
 public:
 
 	AFPSGameMode();
+
+	void CompleteMission(APawn* InstigatorPawn);
+
+	UFUNCTION(BlueprintImplementableEvent, category = "GameMode")
+	void OnMissionCompleted(APawn* InstigatorPawn);
 };
 
 
